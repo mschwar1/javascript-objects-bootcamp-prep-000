be4b58dff24e8b2b7480cf74de23e4e3fdc26f6d
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-16 17:30:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Rihanna: "Umbrella" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
 
